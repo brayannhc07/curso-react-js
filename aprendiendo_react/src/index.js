eb46a2198ce8125f0fd1d612fd54d952db964f7d
Bryan Hernandez <brayannhc07@gmail.com>
2022-01-04 12:49:50 -0600

    Seccion 2: Fin seccion

--- aprendiendo_react/src/index.js
+++ aprendiendo_react/src/index.js
@@ -1,4 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hola Mundo!</h1>, document.getElementById("root"));
+const nombre = "Bryan";
+const color = "blue";
+const sesion = true;
+const pais = null;
+const amigos = ["Saúl", "Itzel", "Fer"];
+
+const JSX = (
+  <div>
+    {sesion === true ? (
+      <div>
+        <h1 className="titulo" style={{ color: color }}>
+          Hola {nombre}!
+        </h1>
+        <p>Que tengas buen día.</p>
+        {pais && <p>Tú eres de {pais}</p>}
+        <ul>
+          {amigos.map((value, index) => (
+            <li key={index}>{value}</li>
+          ))}
+        </ul>
+      </div>
+    ) : (
+      <p>No has iniciado sesión.</p>
+    )}
+  </div>
+);
+
+// const verificarSesion = (sesion) => {
+//   if (sesion == true) {
+//     return JSX;
+//   } else {
+//     return <h1>No has iniciado sesión.</h1>;
+//   }
+// };
+
+// ReactDOM.render(verificarSesion(sesion), document.getElementById("root"));
+ReactDOM.render(JSX, document.getElementById("root"));
